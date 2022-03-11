17d1fb82161ce744aa799eb3e8d822c803eb9fe5
kvrbotkvr <97327478+kvrbotkvr@users.noreply.github.com>
2022-03-11 08:04:12 -0800

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚢 𝚗𝚊𝚖𝚎 , <a href='http://t.me/Kvr_autofilterbot'> 🅲🅸🅽🅴🅼🅰 🅾🆂 </a>, 𝙸𝚝'𝚜 𝚟𝚎𝚛𝚢 𝚎𝚊𝚜𝚢 𝚓𝚞𝚜𝚝 𝚊𝚍𝚍 𝚖𝚎 𝚝𝚘 𝚢𝚘𝚞𝚛 𝚐𝚛𝚘𝚞𝚙 𝚊𝚗𝚍 𝚖𝚊𝚔𝚎 𝚖𝚎 𝚊𝚍𝚖𝚒𝚗,
+𝙼𝚢 𝚗𝚊𝚖𝚎 , <a href='http://t.me/Eva_mariya_manual_bot'> ᴋᴠʀ ʙᴏᴛ </a>, 𝙸𝚝'𝚜 𝚟𝚎𝚛𝚢 𝚎𝚊𝚜𝚢 𝚓𝚞𝚜𝚝 𝚊𝚍𝚍 𝚖𝚎 𝚝𝚘 𝚢𝚘𝚞𝚛 𝚐𝚛𝚘𝚞𝚙 𝚊𝚗𝚍 𝚖𝚊𝚔𝚎 𝚖𝚎 𝚊𝚍𝚖𝚒𝚗,
 𝚃𝚑𝚊𝚝𝚜 𝚊𝚕𝚕 𝙸'𝚕𝚕 𝚙𝚛𝚘𝚟𝚒𝚍𝚎 𝚖𝚘𝚟𝚒𝚎𝚜 𝚝𝚑𝚎𝚛𝚎 ♥️
 """
     HELP_TXT = """𝙷𝙴𝚈 {}
@@ -9,7 +9,7 @@ class script(object):
 ⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆
 ╔═════❰ 𝓒𝓲𝓷𝓮𝓶𝓪 𝓞𝓢 ❱══⪼𒆜
 ║╭━━━━━━━━━━━━━━━⏤͟͟͞͞★ 
-║┣⪼ 𝙼𝚈 𝙽𝙰𝙼𝙴 - <a href="http://t.me/Kvr_autofilterbot"> ᴄɪɴᴇᴍᴀ ᴏꜱ </a>
+║┣⪼ 𝙼𝚈 𝙽𝙰𝙼𝙴 - <a href="http://t.me/Eva_mariya_manual_bot"> ᴋᴠʀ ʙᴏᴛ </a>
 ║┣⪼ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 - <a href="http://t.me/Kvr_admin_bot"> ᴍɪʀꜱʜᴀᴅ </a>
 ║┣⪼ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 - 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ║┣⪼ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ - ᴀᴍᴀʟ
@@ -24,7 +24,7 @@ class script(object):
 - ՏOᑌᖇᑕᗴ ᑕOᗪᗴ - <a href="http://t.me/Kvr_admin_bot"> 𝗖𝗟𝗜𝗖𝗞 𝗛𝗘𝗥𝗘 </a>
 
 𝗠𝗔𝗦𝗧𝗘𝗥:
-<a href="http://t.me/Kvr_admin_bot">  🅲🅸🅽🅴🅼🅰 🅾🆂 </a>"""
+<a href="http://t.me/Kvr_admin_bot">  ᴋᴠʀ ʙᴏᴛ </a>"""
     WHOIS_TXT ="""<b>WHOIS MODULE</b>
 Note:- Give a user details
 
